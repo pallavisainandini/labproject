9f848c6f55f927087e505f85dc892406be52a7f2
pallavisainandini <151799037+pallavisainandini@users.noreply.github.com>
2024-12-09 20:42:48 +0530

    Add files via upload

---
+++ Repository.java
@@ -0,0 +1,5 @@
+package com.exampleprojectlab.labproject.Repository;
+
+public interface Repository {
+
+}
---
+++ Service.java
@@ -0,0 +1,13 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public Order saveOrder(Order order) {
+        return orderRepository.save(order);
+    }
+}
